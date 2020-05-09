981848b36a77cef632876fd82fa4e5da0e11883d
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 13:11:35 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.artist.count
+    self.artists.count
   end
 
   def all_artist_names
